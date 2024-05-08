f3059d27ad409edaffb0274552be722cff07a1ff
Raka G. Kusni A. W.(Raka G. Kusni A. W.)[raka_a] <raka_a@LGENSOL.GLOBAL>
2024-05-08 21:57:26 +0700

    init

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace PingGraf
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer1 = new SplitContainer();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            panel3 = new Panel();
+            button3 = new Button();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            panel4 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            panel3.SuspendLayout();
+            tabControl1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(panel4);
+            splitContainer1.Panel1.Controls.Add(panel3);
+            splitContainer1.Panel1.Controls.Add(panel2);
+            splitContainer1.Panel1.Controls.Add(panel1);
+            splitContainer1.Panel1.RightToLeft = RightToLeft.No;
+            splitContainer1.Panel1.Paint += splitContainer1_Panel1_Paint;
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.RightToLeft = RightToLeft.No;
+            splitContainer1.RightToLeft = RightToLeft.No;
+            splitContainer1.Size = new Size(1040, 457);
+            splitContainer1.SplitterDistance = 138;
+            splitContainer1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(tabControl1);
+            panel1.Location = new Point(406, 5);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(264, 124);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(tableLayoutPanel2);
+            panel2.Location = new Point(4, 5);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(396, 127);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Fill;
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(120, 40);
+            label1.TabIndex = 0;
+            label1.Text = "Target IP/Address";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(129, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(196, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = DockStyle.Fill;
+            label2.Location = new Point(3, 40);
+            label2.Name = "label2";
+            label2.Size = new Size(120, 40);
+            label2.TabIndex = 2;
+            label2.Text = "Interval (ms)";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            label2.Click += label2_Click_1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Dock = DockStyle.Fill;
+            label4.Location = new Point(3, 80);
+            label4.Name = "label4";
+            label4.Size = new Size(120, 44);
+            label4.TabIndex = 4;
+            label4.Text = "Time out (ms)";
+            label4.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(129, 43);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(196, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(129, 83);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(196, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 3;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 37.68116F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 62.31884F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 55F));
+            tableLayoutPanel2.Controls.Add(textBox3, 1, 2);
+            tableLayoutPanel2.Controls.Add(textBox2, 1, 1);
+            tableLayoutPanel2.Controls.Add(label4, 0, 2);
+            tableLayoutPanel2.Controls.Add(label2, 0, 1);
+            tableLayoutPanel2.Controls.Add(textBox1, 1, 0);
+            tableLayoutPanel2.Controls.Add(label1, 0, 0);
+            tableLayoutPanel2.Controls.Add(button1, 2, 1);
+            tableLayoutPanel2.Controls.Add(button2, 2, 2);
+            tableLayoutPanel2.Location = new Point(3, 0);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 3;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 43F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel2.Size = new Size(391, 124);
+            tableLayoutPanel2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(338, 43);
+            button1.Name = "button1";
+            button1.Size = new Size(50, 23);
+            button1.TabIndex = 7;
+            button1.Text = "reset";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(338, 83);
+            button2.Name = "button2";
+            button2.Size = new Size(50, 23);
+            button2.TabIndex = 8;
+            button2.Text = "reset";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button3);
+            panel3.Location = new Point(708, 5);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(83, 124);
+            panel3.TabIndex = 2;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(0, -2);
+            button3.Name = "button3";
+            button3.Size = new Size(54, 82);
+            button3.TabIndex = 0;
+            button3.Text = "Start";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(3, 3);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(261, 121);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(253, 93);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "SaveFile";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(253, 93);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "GraphColor";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Red;
+            panel4.Location = new Point(674, 8);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(28, 23);
+            panel4.TabIndex = 3;
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1040, 457);
+            Controls.Add(splitContainer1);
+            Name = "MainWindow";
+            Text = "Form1";
+            Load += Form1_Load;
+            splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            tabControl1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private Panel panel1;
+        private Panel panel2;
+        private TableLayoutPanel tableLayoutPanel2;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Panel panel3;
+        private Button button3;
+        private Panel panel4;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+    }
+}
---
+++ MainWindow.cs
@@ -0,0 +1,45 @@
+namespace PingGraf
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}

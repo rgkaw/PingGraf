ce9b1a0da5270e941418e2dc177ed2493b356ced
rgkaw <rakasadullah@gmail.com>
2024-08-13 16:38:08 +0700

    change line style

--- MainWindow.cs
+++ MainWindow.cs
@@ -17,21 +17,27 @@ namespace PingGraf
         Queue<int> queue = new Queue<int>();
         Queue<int> queueX = new Queue<int>();
         Series seriesA = new Series();
-
-        public MainWindow()
+        Queue<int> queue123 = new Queue<int>();
+        
+    public MainWindow()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //foreach( int i in Enumerable.Repeat(0, 100).ToArray()) { queue.Enqueue(0); queueX.Enqueue(idx); idx++; };
+            foreach (int i in Enumerable.Repeat(0, 100).ToArray()) {queue123.Enqueue(idx); idx++; };
+            idx = 0;
             chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
             chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = true;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Dot;
+            chart1.ChartAreas[0].AxisX.IsMarksNextToAxis = true;
+         
 
             //Series seriesB = new Series();
             seriesA.Points.DataBindXY(queueX, queue);
             seriesA.ChartType = SeriesChartType.Line;
+            seriesA.IsXValueIndexed = true;
 
             //seriesB.Points.DataBindXY(x,data2);
             //seriesB.ChartType = SeriesChartType.Line;
@@ -77,10 +83,11 @@ namespace PingGraf
             if (PingStatus)
             {
                 PingStatus = false;
-                if (stopToken != null) stopToken.Cancel();
+                if (stopToken != null) { stopToken.Cancel(); panel4.BackColor = Color.Red; }
             }
             else
             {
+                clear();
                 PingStatus = true;
                 stopToken = new CancellationTokenSource();
                 ping_process(stopToken.Token);
@@ -88,7 +95,7 @@ namespace PingGraf
 
         }
 
-        private async void ping_process(CancellationToken cancellationToken = default)
+        private async void ping_process(CancellationToken cancellationToken = default, int delay = 1000)
         {
             try
             {
@@ -105,12 +112,16 @@ namespace PingGraf
                         queue.Dequeue();
                         queueX.Dequeue();
                     }
+                    
                     seriesA.Points.DataBindXY(queueX, queue);
+
                     //chart1.ChartAreas[0].AxisY.Minimum = queue.Min() > 30 ? queue.Min() - 30 : 1;
                     //chart1.ChartAreas[0].AxisY.Minimum = queue.Min() + 30;
+                    chart1.ChartAreas[0].AxisY.MajorGrid.IntervalOffset = 10;
                     chart1.Update();
                     //label3.Text = res.Reply.RoundtripTime.ToString();
                     label3.Text = queue.ToArray().ToString();
+                    if(tmp>0) { panel4.BackColor = Color.Green; } else { panel4.BackColor = Color.Red; }
                     await Task.Delay(1000, cancellationToken);
                 }
             }
@@ -125,6 +136,14 @@ namespace PingGraf
         private void label3_Click_1(object sender, EventArgs e)
         {
 
+        }
+        private void clear()
+        {
+            queue.Clear();
+            queueX.Clear();
+            foreach (int i in Enumerable.Repeat(0, 100).ToArray()) { queue.Enqueue(0); queueX.Enqueue(idx); idx++; };
+            seriesA.Points.Clear();
+            chart1.Update();
         }
     }
 }
